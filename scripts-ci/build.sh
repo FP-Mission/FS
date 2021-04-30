9ccb2e38d16d7195e8b0809f914294e901537160
Charles-Lewis Jaggi <lewisjaggi@gmail.com>
2021-04-30 11:35:57 +0200

    patch actions

--- scripts-ci/build.sh
+++ scripts-ci/build.sh
@@ -25,14 +25,14 @@ fi
 
 #purge an old app if it exists
 cd $1
-echo "purging..."; yes | fprime-util purge $2
+echo "purging..."; yes | fprime-util purge
 
 #now exit on error
 set -e
 
 #build the app
-echo "generating..."; fprime-util generate $2
-echo "building..."; fprime-util build $2
+echo "generating..."; fprime-util generate 
+echo "building..."; fprime-util build 
 
 #start the gds if "start" is an argument
 if [[ "$1" == "start" ]]
