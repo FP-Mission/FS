939ffb831790ee2f74904611ec77002a98cc1693
Jonathan Michel <jonathanmichel@bluewin.ch>
2021-05-25 19:59:36 +0200

    Implement Dictionnary.hpp for ids

---
+++ App/Top/Dictionnary.hpp
@@ -0,0 +1,20 @@
+
+#ifndef __DICTIONNARY_HEADER__
+#define __DICTIONNARY_HEADER__
+
+#define DICT_PR_PingReceived 0x4d
+#define DICT_PiCam_PictureTaken 0x82 
+#define DICT_CommandErrors 0x04
+#define DICT_PR_NumPings 0x4c
+#define DICT_BD_Cycles 0x4e
+#define DICT_Eps_BatteryVoltage 0x64
+#define DICT_TempProb_InternalTemperature 0xa0
+#define DICT_TempProb_ExternalTemperature 0xa1
+// !!!!! TODO Adapt for dev deployment
+#define DICT_THERMOMETER_TEMP 0xa6
+#define DICT_THERMOMETER_HUMI 0xa7
+#define DICT_BAROMETER_TEMP 0xba
+#define DICT_BAROMETER_PRESS 0xbb
+#define DICT_BAROMETER_ALT 0xbc
+
+#endif
