4093d6f904a4bc7a648e04439d29305e03f95b57
Charles-Lewis Jaggi <lewisjaggi@gmail.com>
2021-06-08 19:59:02 +0200

    add dictionnary.hpp

---
+++ App/Top/Dictionnary.hpp
@@ -0,0 +1,20 @@
+
+#ifndef __DICTIONNARY_HEADER__
+#define __DICTIONNARY_HEADER__
+
+#define DICT_PR_PingReceived 0x4d
+#define DICT_PiCam_PictureTaken 0x82 
+#define DICT_CommandErrors 0x04
+#define DICT_PR_NumPings 0x4c
+#define DICT_BD_Cycles 0x4e
+#define DICT_Eps_BatteryVoltage 0x64
+#define DICT_TempProb_InternalTemperature 0xa0
+#define DICT_TempProb_ExternalTemperature 0xa1
+#define DICT_Gps_Position 0x78
+#define DICT_THERMOMETER_TEMP 0xb4
+#define DICT_THERMOMETER_HUMI 0xb5
+#define DICT_BAROMETER_TEMP 0xc8
+#define DICT_BAROMETER_PRESS 0xc9
+#define DICT_BAROMETER_ALT 0xca
+
+#endif
